9637a14fac4b61eb3d0e6f7f2364cd3dfc36557f
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2026-05-07 15:07:32 +0200

    Fix Perl 5.38+ incompatibility in Decode::Base
    Perl 5.38 disallows constant subs that close over lexical variables.
    Replace the per-subclass constant sub approach with a %_params hash
    keyed by subclass package name, driven by a single @_PARAM_NAMES list.
    Add _accessors_installed guard to prevent subroutine redefinition
    warnings when multiple decoder subclasses load the base.

--- lib/Archive/Lha/Decode/Base.pm
+++ lib/Archive/Lha/Decode/Base.pm
@@ -6,53 +6,74 @@ use Carp;
 use Archive::Lha::Constants;
 use Archive::Lha;  # to load XS
 
+my %_params;  # keyed by subclass package name
+
+my @_PARAM_NAMES = qw(
+  NPT
+  NP
+  NT
+  NC
+  PBIT
+  TBIT
+  CBIT
+  PT_TABLE_BIT
+  PT_TABLE_SIZE
+  C_TABLE_BIT
+  C_TABLE_SIZE
+  DICSIZE
+  MAXMATCH
+  THRESHOLD
+);
+
+# Install class methods for each param, looking up via %_params
+for my $name (@_PARAM_NAMES) {
+  no strict 'refs';
+  *{"Archive::Lha::Decode::Base::$name"} = sub { $_params{ ref($_[0]) || $_[0] }{$name} };
+}
+
 sub import {
   my ($class, %options) = @_;
   my $caller = caller;
 
+  my $dicbit        = $options{dicbit}        || 13;
+  my $max_match     = $options{max_match}     || ( 1 << UCHAR_BIT );
+  my $threshold     = $options{threshold}     || 3;
+  my $np            = $options{np}            || $dicbit + 1;
+  my $pbit          = _bit_length( $np );
+  my $pt_table_bit  = $options{pt_table_bit}  || 8;
+  my $c_table_bit   = $options{c_table_bit}   || 12;
+  my $pt_table_size = 1 << $pt_table_bit;
+  my $c_table_size  = 1 << $c_table_bit;
+  my $npt  = $pt_table_size >> 1;
+  my $nt   = USHORT_BIT + 3;
+  my $nc   = UCHAR_MAX  + $max_match + 2 - $threshold;
+  my $tbit = _bit_length( $nt );
+  my $cbit = _bit_length( $nc );
+
+  $_params{$caller} = {
+    NPT           => $npt,
+    NP            => $np,
+    NT            => $nt,
+    NC            => $nc,
+    PBIT          => $pbit,
+    TBIT          => $tbit,
+    CBIT          => $cbit,
+    PT_TABLE_BIT  => $pt_table_bit,
+    PT_TABLE_SIZE => $pt_table_size,
+    C_TABLE_BIT   => $c_table_bit,
+    C_TABLE_SIZE  => $c_table_size,
+    DICSIZE       => 1 << $dicbit,
+    MAXMATCH      => $max_match,
+    THRESHOLD     => $threshold,
+  };
+
   {
-    no strict 'refs'; no warnings 'redefine';
-
-    my $dicbit = $options{dicbit} || 13;
-
-    # should these really be configurable?
-    # XXX: only if we want to support older/rare archives
-    my $max_match = $options{max_match} || ( 1 << UCHAR_BIT );
-    my $threshold = $options{threshold} || 3;
-    my $np        = $options{np}        || $dicbit + 1;
-    my $pbit      = _bit_length( $np );
-
-    my $pt_table_bit  = $options{pt_table_bit} || 8;
-    my $c_table_bit   = $options{c_table_bit}  || 12;
-    my $pt_table_size = 1 << $pt_table_bit;
-    my $c_table_size  = 1 << $c_table_bit;
-
-    # XXX: not so sure if these are right for all
-    my $npt = $pt_table_size >> 1;
-    my $nt  = USHORT_BIT + 3;
-    my $nc  = UCHAR_MAX  + $max_match + 2 - $threshold;
-
-    my $tbit = _bit_length( $nt );
-    my $cbit = _bit_length( $nc );
-
-    *{"$caller\::NPT"}           = sub () { $npt };
-    *{"$caller\::NP"}            = sub () { $np };
-    *{"$caller\::NT"}            = sub () { $nt };
-    *{"$caller\::NC"}            = sub () { $nc };
-    *{"$caller\::PBIT"}          = sub () { $pbit };
-    *{"$caller\::TBIT"}          = sub () { $tbit };
-    *{"$caller\::CBIT"}          = sub () { $cbit };
-    *{"$caller\::PT_TABLE_BIT"}  = sub () { $pt_table_bit };
-    *{"$caller\::PT_TABLE_SIZE"} = sub () { $pt_table_size };
-    *{"$caller\::C_TABLE_BIT"}   = sub () { $c_table_bit };
-    *{"$caller\::C_TABLE_SIZE"}  = sub () { $c_table_size };
-    *{"$caller\::DICSIZE"}       = sub () { 1 << $dicbit };
-    *{"$caller\::MAXMATCH"}      = sub () { $max_match };
-    *{"$caller\::THRESHOLD"}     = sub () { $threshold };
-
-    my @accessors = qw( pt c tree bit );
-    foreach my $name ( @accessors ) {
-      *{"$class\::$name"} = sub { shift->{$name} };
+    no strict 'refs';
+    unless ( ${"$class\::_accessors_installed"} ) {
+      ${"$class\::_accessors_installed"} = 1;
+      for my $name ( qw( pt c tree bit ) ) {
+        *{"$class\::$name"} = sub { shift->{$name} };
+      }
     }
     push @{"$caller\::ISA"}, $class;
   }
@@ -63,27 +84,14 @@ sub new {
 
   my $header = $options{header};
 
-  my $self  = bless {
+  my $self = bless {
     blocksize     => 0,
     read          => $options{read},
     write         => $options{write},
     encoded_size  => $header->{encoded_size},
     original_size => $header->{original_size},
     crc16         => $header->{crc16} || 0,
-    DICSIZE       => $class->DICSIZE,
-    MAXMATCH      => $class->MAXMATCH,
-    THRESHOLD     => $class->THRESHOLD,
-    NPT           => $class->NPT,
-    NP            => $class->NP,
-    NT            => $class->NT,
-    NC            => $class->NC,
-    PBIT          => $class->PBIT,
-    TBIT          => $class->TBIT,
-    CBIT          => $class->CBIT,
-    PT_TABLE_BIT  => $class->PT_TABLE_BIT,
-    PT_TABLE_SIZE => $class->PT_TABLE_SIZE,
-    C_TABLE_BIT   => $class->C_TABLE_BIT,
-    C_TABLE_SIZE  => $class->C_TABLE_SIZE,
+    map { $_ => $class->$_() } @_PARAM_NAMES,
   }, $class;
 
   $self;
